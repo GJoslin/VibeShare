d17018b0e03e830d70813608ae61165434e8e9ed
GJoslin <joslingaddeuk@gmail.com>
2025-08-28 15:25:04 +0100

    updated ad installed standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'i.pravatar.cc' }
     ]
-  },
-  output: 'standalone',
+  }
 };
-
 export default nextConfig;
